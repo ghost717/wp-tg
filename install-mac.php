46c1ec429130687338f0c4f7683a8de26c23b06b
ghost717 <dawid.zawadzki7717@gmail.com>
2020-12-29 15:18:34 +0100

    wp-instaler-beta

---
+++ install-mac.php
@@ -0,0 +1,76 @@
+<?php 
+$break = "\n\n";
+$open = "  ";
+
+$url = "https://wordpress.org/latest.zip";
+$zipFile = "wp.zip";
+
+echo $break;
+echo $open . 'Pobieram ostatnią wersje wordpressa z ' . $url . $break;
+
+echo $break;
+echo $open . ":: Randomowy Kawał ::" . $break;
+echo $open. ":: " . str_replace('"', '', file_get_contents('https://geek-jokes.sameerkumar.website/api'));
+echo $break;
+
+/* DOWNLOAD FILE */
+$zipResource = fopen($zipFile, "w");
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_FAILONERROR, true);
+curl_setopt($ch, CURLOPT_HEADER, 0);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+curl_setopt($ch, CURLOPT_AUTOREFERER, true);
+curl_setopt($ch, CURLOPT_BINARYTRANSFER, true);
+curl_setopt($ch, CURLOPT_TIMEOUT, 10);
+curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+curl_setopt($ch, CURLOPT_FILE, $zipResource);
+$downloaded = curl_exec($ch);
+if (!$downloaded) {
+    echo $open . "<p class='error'>Błąd pobierania - log: ".curl_error($ch) . '</p>';
+}
+curl_close($ch);
+
+/* UNZIP */
+$file = 'wp.zip';
+$path = pathinfo(realpath($file), PATHINFO_DIRNAME);
+$zip = new ZipArchive;
+$res = $zip->open($file);
+if ($res === true) {
+	echo $open . 'Rozpakowuje ' . $file . $break;
+
+    $zip->extractTo($path);
+    $zip->close();
+} else {
+	echo $open . "Nie można było rozpakować archiwum" . $break;
+	echo $open . "Musisz dokończyć proces ręcznie" . $break;
+	exit;
+}
+echo $open . "-Kończenie instalacji-" . $break;
+
+/* CLEANING */
+echo $open . "Usuwam oryginalnego wp-content" . $break;
+exec('rm -rf '.getcwd().'/wordpress/wp-content');
+echo $open . "Usuwam pliki instalacyjne" . $break;
+exec('rm install.php');
+exec('rm install-mac.php');
+echo $open . "Usuwam .gitignore" . $break;
+exec('rm .gitignore');
+echo $open . "Usuwam README.md" . $break;
+exec('rm README.md');
+echo $open . "Kopiuje pliki szablonów oraz motywu" . $break;
+exec('mv wordpress/* .');
+echo $open . "Usuwam repozytorium" . $break;
+exec('rm -rf .git');
+exec('rm wp.zip');
+exec('rm -rf wordpress');
+
+echo $open . "Usuwam license.txt i readme.html" . $break;
+exec('rm license.txt');
+exec('rm readme.html');
+
+echo $break;
+echo $open . "-Operacja zakończona!-";
+
+?>
